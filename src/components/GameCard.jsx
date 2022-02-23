0f30e9ea142dfbc6767edb2d08fbc72de4c4082d
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-02-22 20:04:43 -0800

    Card for each game created and content for home

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,17 +1,31 @@
-import React from "react"
+import React from "react";
+import NumberFormat from 'react-number-format';
+import "../styles/components/gamecard.css"
 
 export class GameCard extends React.Component {
     render() {
         return (
-            <div>
-                <div>
-                    <h1>{this.props.dataFromParent}</h1>
+            <div className="gamecard">
+                <div className="gamecard-container">
+                    <h2>{this.props.gameName}</h2>
                 </div>
-                <div>
-
+                <div className="gamecard-container">
+                    <h4>Active Players</h4>
+                    <NumberFormat
+                        value={this.props.activePlayers}
+                        className="active-players"
+                        displayType={'text'}
+                        thousandSeparator={true}
+                        prefix={''}
+                    />
                 </div>
-                <div>
-
+                <div className="gamecard-container">
+                    <h4>Top Streamers</h4>
+                    <ul>{
+                        this.props.topStreamer.map((streamer, i) => {
+                            return (<li key={streamer + i}>{streamer}</li>)
+                        })
+                    }</ul>
                 </div>
             </div>)
     }
