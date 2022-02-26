6dbe89027d7e165f930ddab2fa9628e30cd86322
Lucas Lee <llee56@mylangara.ca>
2022-02-25 23:23:44 -0800

    Created contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,45 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { ProjectMember } from '../components/ProjectMember';
+
+const propsForCesar = {
+  name: "Cesar Figueroa Socrarras",
+  description: "big brain because salad is named after him",
+  picture: "images/TestImages.jpg"
+}
+
+const propsForLeo = {
+  name: "Leo Chung",
+  description: "can duplicate himself exactly once",
+  picture: "images/TestImages.jpg"
+}
+
+const propsForLucas = {
+  name: "Lucas Lee",
+  description: "high chance of acheiving successful crossdressing",
+  picture: "images/TestImages.jpg"
+}
+
+const propsForMartin = {
+  name: "Martin Wong",
+  description: "absolute gamer and chess champion",
+  picture: "images/TestImages.jpg"
+}
 
 function Contact() {
     return (
       <div>
           <Header />
           <div className="content-page">
+            <article className="project-members">
+              <ProjectMember {...propsForCesar} />
+              <ProjectMember {...propsForLeo} />
+              <ProjectMember {...propsForLucas} />
+              <ProjectMember {...propsForMartin} />  
+            </article>
           
-        </div>
+
+          </div>
           <Footer/>
       </div>
     );
