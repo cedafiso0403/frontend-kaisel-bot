29ab7058c46e07c9bc537b720669643df46bc7af
Lucas Lee <llee56@mylangara.ca>
2022-02-27 11:54:13 -0800

    Edited home page HTML and CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,72 +27,60 @@ function Home() {
   return (
     <div>
       <Header />
-      <div className="content-page-background">
-      </div>
-      <div className="content-page">
-      <div className="home-icon">
-        <img 
-          alt="Kaisel Circle" 
-          src="images/KaiselCircle2.png"
-          height="75px"
-          width="75px" 
-        />
-        <h1 className="content-h1">PROJECT KAISEL</h1>
-      </div>
-        <p className="home-subtitle">
-          All-in-one Discord gaming bot
-        </p>
-        <p className="features-subtitle">
-          Integrated with the Riot games and Twitch API,
-          Kaisel<br></br> supports statistics for <a href="https://www.leagueoflegends.com/" target="_blank" rel="noreferrer" >
-          League of Legends</a>, <a href="https://teamfighttactics.leagueoflegends.com/en-us/" target="_blank" rel="noreferrer">
-          TFT</a> and <a href="https://playvalorant.com/en-us/" target="_blank" rel="noreferrer">VALORANT</a>.
-          <br></br>
-          Access statistics for supported games, Twitch alerts, server moderation, and more!
-        </p>
+      {/* <div className="content-page-background">
+      </div> */}
+      <main className="content-page">
+        <div className="home-icon">
+          <img 
+            alt="Kaisel Circle" 
+            src="images/KaiselCircle2.png"
+            height="75px"
+            width="75px" 
+          />
+          <h1 className="content-h1">PROJECT KAISEL</h1>
+        </div>
+        <div>
+          <p className="home-subtitle">
+            All-in-one Discord gaming bot
+          </p>
+
+          <p className="features-subtitle">
+            Integrated with the Riot games and Twitch API,
+            Kaisel<br></br> supports statistics for <a href="https://www.leagueoflegends.com/" target="_blank" rel="noreferrer" >
+            League of Legends</a>, <a href="https://teamfighttactics.leagueoflegends.com/en-us/" target="_blank" rel="noreferrer">
+            TFT</a> and <a href="https://playvalorant.com/en-us/" target="_blank" rel="noreferrer">VALORANT</a>.
+            <br></br>
+            Access statistics for supported games, Twitch alerts, server moderation, and more!
+          </p>  
+        </div>
+        
         <div className="gamecards">
           <GameCard {...propsForGameCardLeague} />
           <GameCard {...propsForGameCardTFT} />
           <GameCard {...propsForGameCardValorant} />
         </div>
 
-
-        <div className="alt-content-page marginBottom">
-
-        <div className="content-space"></div>
-
-        <Twitch />
-
-        <div className="content-space"></div>
-        <div className="content-space"></div>
-
-        <h2 className="content-h2">See the progress in your games</h2>
-        <Features />
-
-        <div className="content-space"></div>
-        <div className="content-space"></div>
-
-        <h2 className="content-h2">Search for a username</h2>
-        <p className="features-subtitle">
-          Test out how Kaisel can display your in game statistics.
-          <br></br>
-          View your KDA, winrate and other statistics for the current season.
-        </p>
-        <SearchBar />
+        <div>
+          <h2 className="content-h2">Search for a username</h2>
+          <p className="features-subtitle">
+            Test out how Kaisel can display your in game statistics.
+            <br></br>
+            View your KDA, winrate and other statistics for the current season.
+          </p>
+          <SearchBar />  
         </div>
         
-        <div className="container">
-          <div>
-            <h2 className="content-h2">See the progress in your games</h2>
-            <Features />
-          </div>
+        <div>
+          <Twitch />
         </div>
 
-          <div className="alt-content-page">
-            <Twitch />
-          </div>
-
-      </div>
+        <div>
+          <h2 className="content-h2">See the progress in your games</h2>
+          <Features />  
+        </div>
+      
+        
+      </main>
       <Footer />
     </div>
   );
