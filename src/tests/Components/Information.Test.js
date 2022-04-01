c2201355aa4270ab7a341cacd7167c30d04d4ae1
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-04-01 16:43:55 -0700

    Unit testing program for TwitchIntro element, fixed descriptions for Information tests

--- src/tests/Components/Information.Test.js
+++ src/tests/Components/Information.Test.js
@@ -7,19 +7,19 @@ test('Render without crashing', ()=>{
     render(<Information />)
 })
 
-test('Render learn more heading', ()=>{
+test('Render first line', ()=>{
     render(<Information />)
     const line1 = screen.getByText("Kaisel lets you search up any username from").closest('p');
     expect(line1).toBeInTheDocument();
 })
 
-test('Render learn more heading', ()=>{
+test('Render second line', ()=>{
     render(<Information />)
     const line2 = screen.getByText("league of legend or valorant that shows your").closest('p');
     expect(line2).toBeInTheDocument();
 })
 
-test('Render learn more heading', ()=>{
+test('Render third line', ()=>{
     render(<Information />)
     const line3 = screen.getByText("stats like rank, kda, win rate, and games played.").closest('p');
     expect(line3).toBeInTheDocument();
