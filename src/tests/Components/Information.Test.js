62b1976061df6edad3c654426568256c3e5c2b6e
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-04-01 16:36:05 -0700

    Unit testing program for Information component

---
+++ src/tests/Components/Information.Test.js
@@ -0,0 +1,27 @@
+import Information from "../../components/Information";
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom';
+
+
+test('Render without crashing', ()=>{
+    render(<Information />)
+})
+
+test('Render learn more heading', ()=>{
+    render(<Information />)
+    const line1 = screen.getByText("Kaisel lets you search up any username from").closest('p');
+    expect(line1).toBeInTheDocument();
+})
+
+test('Render learn more heading', ()=>{
+    render(<Information />)
+    const line2 = screen.getByText("league of legend or valorant that shows your").closest('p');
+    expect(line2).toBeInTheDocument();
+})
+
+test('Render learn more heading', ()=>{
+    render(<Information />)
+    const line3 = screen.getByText("stats like rank, kda, win rate, and games played.").closest('p');
+    expect(line3).toBeInTheDocument();
+})
+
