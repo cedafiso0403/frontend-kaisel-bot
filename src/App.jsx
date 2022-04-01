e9972581801be67ec7c16db06f73f549591d623e
Mightyy <59546774+Mighty303@users.noreply.github.com>
2022-04-01 00:54:26 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
           <Route exact path="/Twitch" element={<Twitch />}></Route>
           <Route path="/TestStats/*" element={<Stats />}></Route>
           <Route path="*" element={<NotFound />}></Route>
+      </div>
         </Routes>
     </BrowserRouter>
   );
