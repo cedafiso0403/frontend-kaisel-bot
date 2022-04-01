a43fb90e761b966607ba9f48b260acfd308cc670
Mightyy <59546774+Mighty303@users.noreply.github.com>
2022-03-31 19:44:03 -0700

    fixed bugs

--- src/components/twitch/TwitchFeatured.jsx
+++ src/components/twitch/TwitchFeatured.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactTwitchEmbedVideo from "react-twitch-embed-video";
-// import { useEffect } from 'react';
 
 const thumbnail_width = '1000'; 
 const thumbnail_height = '500'; 
@@ -17,7 +16,12 @@ const TwitchStreamers = props => {
                 <h1 className="content-h2">Featured Streamer</h1>
                 <h2 className="twitch-featured-h2">{props.featuredStreamer.user_name} is playing {props.featuredStreamer.game_name}</h2>
                 <div className="twitch-thumbnail">
-                    <ReactTwitchEmbedVideo channel={props.featuredStreamer.user_name} theme="dark" width={thumbnail_width} height={thumbnail_height}/>
+                    <ReactTwitchEmbedVideo 
+                        channel={props.featuredStreamer.user_login} 
+                        theme="dark" 
+                        width={thumbnail_width} 
+                        height={thumbnail_height}
+                    />
                 </div>
                 <p>{shortenTitle(props.featuredStreamer.title)}</p>
                 <div className="viewer-container">
