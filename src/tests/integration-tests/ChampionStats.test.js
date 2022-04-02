c6d8069b5fcea409c7875ffe97091cbfc6bfedf8
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-04-01 19:45:58 -0700

    Test Riot API feature one

--- src/tests/integration-tests/ChampionStats.test.js
+++ src/tests/integration-tests/ChampionStats.test.js
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ChampionStats from "../../components/ChampionStats";
 import '@testing-library/jest-dom';
 
@@ -34,48 +34,51 @@ const formatResult = (dataToFormat) => {
     }
 }
 
-test('Renders without crashing',()=>{
-    render(<ChampionStats {...props}/>);
-});
+describe("Testing RIOT API Feature #1", () => {
+    test('Renders without crashing', () => {
+        render(<ChampionStats {...props} />);
+    });
 
-test('Renders image for most played champ',()=>{
-    render(<ChampionStats {...props}/>);
-    const imgChamp = screen.getByAltText("Most played champ").closest('img');
-    expect(imgChamp).toBeInTheDocument();
-});
+    test('Renders image for most played champ', () => {
+        render(<ChampionStats {...props} />);
+        const imgChamp = screen.getByAltText("Most played champ").closest('img');
+        expect(imgChamp).toBeInTheDocument();
+    });
 
-test('Renders name for most played champ',()=>{
-    render(<ChampionStats {...props}/>);
-    const nameChamp = screen.getByText(props.championName).closest('p');
-    expect(nameChamp).toBeInTheDocument();
-});
+    test('Renders name for most played champ', () => {
+        render(<ChampionStats {...props} />);
+        const nameChamp = screen.getByText(props.championName).closest('p');
+        expect(nameChamp).toBeInTheDocument();
+    });
 
-test('Renders team position for most played champ',()=>{
-    render(<ChampionStats {...props}/>);
-    const teamPositionChamp = screen.getByText(formatResult(props.teamPosition)).closest('p');
-    expect(teamPositionChamp).toBeInTheDocument();
-});
+    test('Renders team position for most played champ', () => {
+        render(<ChampionStats {...props} />);
+        const teamPositionChamp = screen.getByText(formatResult(props.teamPosition)).closest('p');
+        expect(teamPositionChamp).toBeInTheDocument();
+    });
 
-test('Renders kills average for most played champ',()=>{
-    render(<ChampionStats {...props}/>);
-    const killAverageChamp = screen.getByText((props.kills/props.matchPlayed).toFixed(1)).closest('p');
-    expect(killAverageChamp).toBeInTheDocument();
-});
+    test('Renders kills average for most played champ', () => {
+        render(<ChampionStats {...props} />);
+        const killAverageChamp = screen.getByText((props.kills / props.matchPlayed).toFixed(1)).closest('p');
+        expect(killAverageChamp).toBeInTheDocument();
+    });
 
-test('Renders death average for most played champ',()=>{
-    render(<ChampionStats {...props}/>);
-    const deathAverageChamp = screen.getByText((props.deaths/props.matchPlayed).toFixed(1)).closest('p');
-    expect(deathAverageChamp).toBeInTheDocument();
-});
+    test('Renders death average for most played champ', () => {
+        render(<ChampionStats {...props} />);
+        const deathAverageChamp = screen.getByText((props.deaths / props.matchPlayed).toFixed(1)).closest('p');
+        expect(deathAverageChamp).toBeInTheDocument();
+    });
 
-test('Renders assist average for most played champ',()=>{
-    render(<ChampionStats {...props}/>);
-    const assistAverageChamp = screen.getByText((props.assists/props.matchPlayed).toFixed(1)).closest('p');
-    expect(assistAverageChamp).toBeInTheDocument();
-});
+    test('Renders assist average for most played champ', () => {
+        render(<ChampionStats {...props} />);
+        const assistAverageChamp = screen.getByText((props.assists / props.matchPlayed).toFixed(1)).closest('p');
+        expect(assistAverageChamp).toBeInTheDocument();
+    });
 
-test('Renders CS average for most played champ',()=>{
-    render(<ChampionStats {...props}/>);
-    const csAverageChamp = screen.getByText((props.totalMinionsKilled/props.matchLength).toFixed(1)).closest('p');
-    expect(csAverageChamp).toBeInTheDocument();
-});
+    test('Renders CS average for most played champ', () => {
+        render(<ChampionStats {...props} />);
+        const csAverageChamp = screen.getByText((props.totalMinionsKilled / props.matchLength).toFixed(1)).closest('p');
+        expect(csAverageChamp).toBeInTheDocument();
+    });
+})
+
