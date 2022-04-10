c1755592b096e795edc0c2fc73355dc20e43282f
cedafiso0403 <cfigueroasocar00@mylangara.ca>
2022-04-10 15:11:59 -0700

    Step for requesting Production Key

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Stats from './pages/Stats';
 import NotFound from './pages/NotFound';
 import Twitch from './pages/Twitch';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Riot from './riot.txt'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/Contact" element={<Contact />}></Route>
           <Route exact path="/Statistics" element={<Stats />}></Route>
           <Route exact path="/Twitch" element={<Twitch />}></Route>
+          <Route path="/riot.txt" element={"5d74104f-5dac-4e5f-8595-473c31d3a478"}></Route>
           <Route path="/TestStats/*" element={<Stats />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
